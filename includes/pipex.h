8e73da733d8357881693dcfa230a030c1bdb3c62
jjuanramos <jjuanramos91@gmail.com>
2024-02-23 12:56:15 +0100

    get rid of stdlib because we'll fuck up and we know it

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:35:27 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/23 12:50:34 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/23 12:55:34 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 
 # include "include/ft_printf.h"
 # include "include/libft.h"
-# include <stdlib.h>
 
 #endif
