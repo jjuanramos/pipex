58b39e14d7d82645209c11b51bb65f3fdf72203f
jjuanramos <jjuanramos91@gmail.com>
2024-03-19 11:43:29 +0100

    back to beginning

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:35:27 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/23 13:13:34 by juramos          ###   ########.fr       */
+/*   Updated: 2024/03/19 11:26:20 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include "include/ft_printf.h"
 # include "include/libft.h"
 # include <unistd.h>
+# include <errno.h>
+# include <string.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,20 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 12:49:59 by juramos           #+#    #+#             */
-/*   Updated: 2024/03/11 11:18:34 by juramos          ###   ########.fr       */
+/*   Updated: 2024/03/19 11:43:09 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "pipex.h"
 
 void	leaks(void)
 {
 	system("leaks -q pipex");
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	atexit(leaks);
-	return (0);
+	char	**commands;
+	char	**archives;
+
+	if (argc == 5)
+	{
+		commands = get_commands(argv);
+		archives = get_archives(argv);
+		if (!commands || !archives)
+			print_and_exit(2, "Failed during the retrieval of args\n", 1);
+	}
 }
